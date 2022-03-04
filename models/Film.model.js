a305e17e54d24fc83932c77b1c302369645187d0
NewBie-profesion <ptd012001@gmail.com>
2022-03-04 20:48:27 +0700

    create model Film , User , Ticket

--- models/Film.model.js
+++ models/Film.model.js
@@ -1,3 +1,52 @@
 const mongoose = require('mongoose');
 
-module.exports = mongoose.model('Film');
+const FilmSchema = new mongoose.Schema({
+  IDFilm: {
+    type: Object.id,
+  },
+  nameFilm: {
+    type: String,
+    required: [true, 'Tên Phim không được để trống'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Nội dung phim không được để trống'],
+  },
+  director: {
+    type: String,
+    required: [true, 'Đạo diễn của phim không được để trống'],
+  },
+  country: {
+    type: String,
+    required: [true, 'Quốc gia không được để trống'],
+  },
+  category: {
+    type: String,
+    required: [true, 'Thể loại phim không được bỏ trống'],
+  },
+  actor: {
+    type: Array,
+    required: [true, 'Diễn viên chính của phim không được để trống'],
+  },
+  cinema: {
+    type: String,
+    required: [true, 'Rạp chiếu phim không được để trống'],
+  },
+  movieDay: {
+    type: Date,
+    required: [true, 'Ngày chiếu phim không được để trống'],
+  },
+  showTime: {
+    type: String,
+    required: [true, 'Khung giờ chiếu phim không được để trống'],
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updateAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model('Film', FilmSchema);
---
+++ models/Ticket.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const TicketSchema = new mongoose.Schema({
+  seat: {
+    type: String,
+    required: [true, 'Mời nhập chỗ ngồi'],
+  },
+  room: {
+    type: String,
+    required: [true, 'Mời nhập phòng'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Mời nhập giá vé'],
+  },
+  IDFilm: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', TicketSchema);
