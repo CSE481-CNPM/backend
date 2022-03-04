606b3e1679a5c2c6649d532406b2c567780b2334
Lê Thanh Sơn <thanhson0514@gmail.com>
2022-03-04 13:13:48 +0700

    add packages for authentication

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./helpers/logger');
 const connectDabase = require('./config/database');
+const cors = require('cors');
+
+// import router
+const filmRouter = require('./routes/film.route');
+const authRouter = require('./routes/auth.route');
 
 // process config
 const app = express();
 dotenv.config();
 connectDabase();
 
-// import router
-const filmRouter = require('./routes/film.route');
-const authRouter = require('./routes/auth.route');
-
+app.use(cors());
 app.use(express.json());
+
 app.use('/api/v1/film', filmRouter);
 app.use('/api/v1/auth', authRouter);
 
