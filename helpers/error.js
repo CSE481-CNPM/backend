2ac5a82243dd8bc7aef68b9285939b7da1c7eee4
Lê Thanh Sơn <thanhson0514@gmail.com>
2022-03-06 01:19:37 +0700

    catch Cast Error and Duplicate in mongodb

---
+++ helpers/error.js
@@ -0,0 +1,29 @@
+const logger = require('./logger');
+const ErrorHandler = require('../utils/errorResponse');
+const ErrorResponse = require('../utils/errorResponse');
+
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
+  logger.error('stack:', err);
+
+  // Bắt lỗi trùng kháo trong Database
+  if (err.code === 11000) {
+    const message = 'Giá trị của trường bị trùng lặp';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // bắt lỗi xác nhận các trường thông tin
+  if (err.name === 'ValidationError') {
+    const message = Object.values(error.errors).map((value) => value.message);
+    error = new ErrorHandler(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || 'Lỗi hệ thống'
+  });
+};
+
+module.exports = errorHandler;
