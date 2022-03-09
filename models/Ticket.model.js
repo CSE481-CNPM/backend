83610843608b70d3ac114bf678a9b7a878dc1dc5
Lê Thanh Sơn <thanhson0514@gmail.com>
2022-03-09 23:58:22 +0700

    fix model

--- models/Ticket.model.js
+++ models/Ticket.model.js
@@ -1,47 +1,33 @@
 const mongoose = require('mongoose');
 
-const TicketSchema = new mongoose.Schema({
-  seat: {
-    type: String,
-    required: [true, 'Mời nhập chỗ ngồi']
-  },
-  isActive: {
-    type: Boolean,
-    default: false
-  },
-  room: {
-    type: String,
-    required: [true, 'Mời nhập phòng']
-  },
-  price: {
-    type: Number,
-    required: [true, 'Mời nhập giá vé']
-  },
-  premiereSchedule: [
-    {
-      cinema: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Cinema'
-      },
-      time: [
-        {
-          type: String
-        }
-      ]
+const TicketSchema = new mongoose.Schema(
+  {
+    seat: {
+      type: String,
+      required: [true, 'Mời nhập chỗ ngồi']
+    },
+    room: {
+      type: String,
+      required: [true, 'Mời nhập phòng']
+    },
+    price: {
+      type: Number,
+      required: [true, 'Mời nhập giá vé']
+    },
+    showTime: {
+      type: String,
+      require: true
+    },
+    filmId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Film'
+    },
+    cinemaId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Cinema'
     }
-  ],
-  filmId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Film'
   },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Ticket', TicketSchema);
