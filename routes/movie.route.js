3ff80e1c3ffbb2c9019052caa40ef7e71ca1d34c
Lê Thanh Sơn <thanhson0514@gmail.com>
2022-03-10 22:02:39 +0700

    update router movie

--- routes/movie.route.js
+++ routes/movie.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { get } = require('../controllers/movie.controller');
+const { getAll, create } = require('../controllers/film.controller');
+const { protect, hasAuthorization } = require('../middleware/auth');
 
-router.get('/', get);
+router.get('/', getAll);
+router.post('/', protect, hasAuthorization('admin'), create);
 
 module.exports = router;
